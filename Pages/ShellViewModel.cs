25920d89b51dd80145b9c2c56dc3764adec80173
trustie <mariocatch@gmail.com>
2020-07-27 17:10:52 -0400

    .

---
+++ Pages/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Stylet;
+
+namespace Company.WpfApplication1.Pages
+{
+    public class ShellViewModel : Screen
+    {
+        private string username;
+
+        public string Username { get => username; set => SetAndNotify(ref username, value, nameof(Username)); }
+
+        public void Do() { }
+
+        public bool CanDo => !string.IsNullOrWhiteSpace(username);
+    }
+}
